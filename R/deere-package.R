65e1687fcbb43a134cb1c36939c7c3d3c73c30a0
hrbrmstr <bob@rud.is>
2019-06-10 09:46:38 -0400

    R package repo initialization complete

---
+++ R/deere-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/deere>
+#' - BugReports: <https://gitlab.com/hrbrmstr/deere/issues>
+#' 
+#' @md
+#' @name deere
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
